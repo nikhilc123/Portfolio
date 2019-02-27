0ed3aac32c39c8328d719e53b90c43e121bab7f6
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-02-27 14:26:07 -0500

    Created sample data for Portfolio, Skill and blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Load sample blog data
+10.times do |blog|
+  Blog.create!(
+          title:  "My sample blog #{blog}",
+          body:   "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
+                  content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their
+                  default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over
+                  the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+          )
+end
+
+puts "Sample Blogs loaded"
+
+#Load sample skills
+5.times do |skill|
+  Skill.create!(
+           title:           "My awesome #{skill}",
+           percent_utilized: 75
+  )
+end
+
+puts "Sample Skills loaded"
+
+9.times do |portfolio_skills|
+  Portvolio.create!(
+               title:       "My Portfolio #{portfolio_skills}",
+               subtitle:    "My great service",
+               body:        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
+                            making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more
+                            obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                            classical literature, discovered the undoubtable source.",
+               main_image:  "https://via.placeholder.com/150X200",
+               thumb_image: "https://via.placeholder.com/250x200"
+  )
+
+  puts "Portfolio samples created"
+end
