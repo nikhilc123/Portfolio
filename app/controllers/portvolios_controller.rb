130b1cca117187c17fd901f5ac7fb55ef8cd39d7
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-02-27 15:44:33 -0500

    Added create and new actions to Portfolio

--- app/controllers/portvolios_controller.rb
+++ app/controllers/portvolios_controller.rb
@@ -1,5 +1,26 @@
 class PortvoliosController < ApplicationController
   def index
-    @portvolio_items = Portvolio.all
+    @portfolio_items = Portvolio.all
+  end
+
+  def new
+    @portfolio_item = Portvolio.new
+  end
+
+  def create
+    @portfolio_item = Portvolio.new(portfolio_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portvolios_path, notice: 'Your portfolio is new live!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portvolio).permit(:title, :subtitle, :body)
   end
 end
