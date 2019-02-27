a3b15d66288b3600bfd660ee0c28b068ce4024d2
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-02-27 16:58:37 -0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :portvolios
+  get 'portvolio/:id', to: 'portvolios#show', as: 'portfolio_show'
 
   root 'pages#home'
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
 
   get 'contact', to: 'pages#contact'
 
