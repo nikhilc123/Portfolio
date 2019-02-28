7147ee1fccd3e97518ef3452e79d1def48a6c05f
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-02-27 21:45:40 -0500

    Added validations to blog, portfolio, skill

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  validates_presence_of :title, :body
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
--- app/models/portvolio.rb
+++ app/models/portvolio.rb
@@ -1,2 +1,3 @@
 class Portvolio < ApplicationRecord
+  validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 end
