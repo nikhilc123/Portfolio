0ebb33a9a7f3e249dc28071dbd3c659d46318831
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-02-28 19:07:07 -0500

    Implemented null object pattern

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,13 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  def current_user
+    #import all the current implementation of user by using super
+    # null object pattern, in view you do not have to check if user is signed in
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "Guest", first_name: "Guest", last_name: "User", email: "guest@example.com")
+  end
+end
