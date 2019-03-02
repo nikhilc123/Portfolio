803c7c0985f310d2f61ceb777c3de032e7d1005b
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-01 19:54:34 -0500

    Style along helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     #check if the class is User or OpenStruct
     # two ways: is_a?(OpenStruct) or User OR simply pass User model
     # concat because ruby always return last statement
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path) +
-      ("<br/>".html_safe) +
-      (link_to "Register", new_user_registration_path)
+      (link_to "Login", new_user_session_path, class: style) +
+      (" ".html_safe) +
+      (link_to "Register", new_user_registration_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
