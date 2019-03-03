b755029c582e31cde9510a4260b52484c7701907
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-03 12:55:41 -0500

    Removed Placeholder inside concern, reorganized placeholder inside portfolio helper, updated portfolio index and show page, removed placeholder from skill model

--- app/models/portvolio.rb
+++ app/models/portvolio.rb
@@ -3,22 +3,14 @@ class Portvolio < ApplicationRecord
   # save attributes on associated records through the parent
   # Learn more https://api.rubyonrails.org/classes/ActiveRecord/NestedAttributes/ClassMethods.html
   accepts_nested_attributes_for :technologies, reject_if: lambda { |attr| attr['name'].blank? }
-  include Placeholder
   validates_presence_of :title, :subtitle, :body
 
   scope :ror_angular_items, -> { where(subtitle: "Angular") }
 
-  after_initialize :set_defaults
 
   mount_uploader :thumb_image, PortvolioUploader
   mount_uploader :main_image, PortvolioUploader
 
-  def set_defaults
-    # || because if you are editing a file you don't want to override
-    # check if it exist, if nil then url
-    self.main_image ||= Placeholder.image_generator(height: 400, width: 400)
-    self.thumb_image ||= Placeholder.image_generator(height: 200, width: 200)
-  end
 
   def self.by_position
     order('position ASC')
