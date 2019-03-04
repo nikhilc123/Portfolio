cd167bbcd5a3154edaa6df6edd0840fa61623317
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-04 15:53:44 -0500

    Created a comment broadcast job and update the configuration inside routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portvolios, except: [:show] do
     put 'sort', on: :collection
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
 end
