ec3b8100cb84187a3cf992efe5ba1b849b484a2b
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-06 15:44:37 -0500

    Implemented production call for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ["https://nikhilc-portfolio.herokuapp.com",
+                                                  "http://nikhilc-portfolio.herokuapp.com"]
+
+  config.action_cable.url = "wss://nikhilc-portfolio.herokuapp.com"
 end
