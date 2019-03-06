ef55779bb4a2a374f907cf4f78b20f45627904e9
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-06 02:04:18 -0500

    Added assets path to application file to render the video on homepage

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Portfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
